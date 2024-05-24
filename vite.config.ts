bb5ab676df2eecd60af4c59420aa68702d0ae3fb
Camilo López <camilolopez1506@gmail.com>
2024-05-24 13:00:01 -0500

    change vite

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,7 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-  base: '/',  // Configura correctamente la base path
   build: {
     outDir: 'dist',
-    rollupOptions: {
-      output: {
-        assetFileNames: '[name].[hash].[ext]'
-      }
-    }
   },
-  publicDir: 'public'
 })
