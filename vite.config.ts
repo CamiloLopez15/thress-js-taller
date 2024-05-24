c514bd11a5ffb9b9d10b9ac69ab440eb50e32821
Camilo López <camilolopez1506@gmail.com>
2024-05-24 13:01:34 -0500

    change vercel json and vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,14 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  base: '/',  // Configura correctamente la base path
   build: {
     outDir: 'dist',
+    rollupOptions: {
+      output: {
+        assetFileNames: '[name].[hash].[ext]'
+      }
+    }
   },
+  publicDir: 'public'
 })
